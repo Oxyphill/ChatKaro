cd698b37a38c30a69c1c34af6cedc80a42251b0d
saikiran <saikiran.vf81@gmail.com>
2018-10-14 21:06:52 -0700

    Base line version of Server side code is added

---
+++ Server.d/server.h
@@ -0,0 +1,18 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+
+#define TRUE   1 
+#define FALSE  0 
+#define PORT 8888 
+#define max_clients 30
+
+
+
+
+//Function Prototypes
+void * connection_handle(void * data);
+
+
+
+#endif
